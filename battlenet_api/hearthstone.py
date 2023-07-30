e6f08b72b1bb321952b5b4c21e4084cf80badba8
smg <alsrl8@naver.com>
2023-07-31 02:50:52 +0900

    Handle Hearthstone API's request url with common function

--- battlenet_api/hearthstone.py
+++ battlenet_api/hearthstone.py
@@ -4,25 +4,41 @@ import enums
 base_url = 'https://us.api.blizzard.com/hearthstone/'
 
 
-def get_hearthstone_metadata(token: str, category: enums.MetadataCategory = enums.MetadataCategory.NONE, locale: str = 'ko_KR'):
+class CardApiParameter:
+    def __init__(self, token: str, page: int = 1, page_size: int = 1, locale: str = 'ko_KR'):
+        self.token = token
+        self.page = page
+        self.page_size = page_size
+        self.locale = locale
+
+
+def add_common_query_parameters(url: str, common_param: CardApiParameter):
+    return f'{url}?access_token={common_param.token}&locale={common_param.locale}&page={common_param.page}&pageSize={common_param.page_size}'
+
+
+def get_hearthstone_metadata(common_param: CardApiParameter, category: enums.MetadataCategory = enums.MetadataCategory.NONE):
     metadata_request_url = base_url + 'metadata' + (f'/{category.value}' if category != enums.MetadataCategory.NONE else '')
-    response = requests.get(f'{metadata_request_url}?access_token={token}&locale={locale}')
+    metadata_request_url = add_common_query_parameters(metadata_request_url, common_param)
+    response = requests.get(metadata_request_url)
     return response.json()
 
 
-def get_card_by_id(card_id: int, token: str, locale: str = 'ko_KR'):
-    card_request_url = base_url + 'cards'
-    response = requests.get(f'{card_request_url}/{card_id}?access_token={token}&locale={locale}')
+def get_card_by_id(common_param: CardApiParameter, card_id: int):
+    card_request_url = base_url + f'cards/{card_id}'
+    card_request_url = add_common_query_parameters(card_request_url, common_param)
+    response = requests.get(card_request_url)
     return response.json()
 
 
-def get_all_cards(token: str, locale: str = 'ko_KR'):
+def get_all_cards(common_param: CardApiParameter):
     card_request_url = base_url + 'cards'
-    response = requests.get(f'{card_request_url}?access_token={token}&locale={locale}')
+    card_request_url = add_common_query_parameters(card_request_url, common_param)
+    response = requests.get(card_request_url)
     return response.json()
 
 
-def get_all_cards_by_game_mode(token: str, game_mode: enums.GameMode = enums.GameMode.CONSTRUCTED, locale: str = 'ko_KR', page: int = 1):
+def get_all_cards_by_game_mode(common_param: CardApiParameter, game_mode: enums.GameMode = enums.GameMode.CONSTRUCTED):
     card_request_url = base_url + 'cards'
-    response = requests.get(f'{card_request_url}?gameMode={game_mode.value}&access_token={token}&locale={locale}&page={page}')
+    card_request_url = add_common_query_parameters(card_request_url, common_param)
+    response = requests.get(f'{card_request_url}&gameMode={game_mode.value}')
     return response.json()
